058d2c45106a339fe8bfcbe8dfac8f85043896bc
John King <john@mettaaudio.com>
2013-03-07 11:04:44 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FairfieldGroupRealtors::Application.config.secret_token = '600bf37bafc263bd7f60053afda57d856d3c85171721eddad19a8bc22f7df2ed69bc25e80f6edce2e32c8ac34a377e08230e0cec3c718a01ece95dc471170785'
